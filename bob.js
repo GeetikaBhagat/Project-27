73470aa680d2bcafbfe4ef1a7616e978fb0a0159
wakcream <71416144+wakcream@users.noreply.github.com>
2021-01-17 18:54:26 -0700

    Add files via upload

--- bob.js
+++ bob.js
@@ -2,20 +2,22 @@ class Bob {
     constructor(x, y,radius) {
       var options = {
         isStatic: false,
-        restitution: 0.3,
-        friction: 0.5,
-        density: 1.2
+        restitution: 1,
+        friction: 0,
+        density: 0.8
       }
       this.body = Bodies.circle(x,y,radius,options,0);
       this.radius = radius;
       World.add(world,this.body);
     }
     display(){
-      push();
       var pos = this.body.position;
-      translate(pos.x,pos.y);
+      push();
+      translate(this.body.position.x,this.body.position.y);
       fill("white");
-      circle(pos.x,pos.y,this.radius);
+      ellipseMode(RADIUS);
+      ellipse(0,0,this.radius);
+      circle(0,0,this.radius);
       pop();
     }
   }
